84108dbf4fa8601440cb0bc6f48bb187dd3fcd24
Gruak <gruak00@gmail.com>
2018-10-11 15:34:30 +0200

    Kolejny commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const _ = require('loadash');
 
-const oceny = [5, 4, 3, 2, 3];
+const oceny = [5, 4, 3, 2, 3, 5, 2, 6];
 
-console.log('Hello', .mean(oceny));
+console.log('Hello', _.mean(oceny));
+console.log('To jest drugi commit');
